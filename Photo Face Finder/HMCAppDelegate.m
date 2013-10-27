2a0d65b0dfba7cf3e9d436edc7cbe7608d5b76b9
Adam Stegman <astegman@gmail.com>
2013-10-26 22:36:48 -0700

    Selected a directory of photos
    Just logging it out for now.

--- Photo Face Finder/HMCAppDelegate.m
+++ Photo Face Finder/HMCAppDelegate.m
@@ -1,10 +1,29 @@
 #import "HMCAppDelegate.h"
 
+@interface HMCAppDelegate ()
+- (NSURL *)requestPhotoDirectory;
+@end
+
 @implementation HMCAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
   // Insert code here to initialize your application
+  NSURL *directory = [self requestPhotoDirectory];
+  NSLog(@"Chose directory: %@", [directory description]);
+  // TODO: handle nil
+}
+
+- (NSURL *)requestPhotoDirectory {
+  NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+  openPanel.canChooseDirectories = YES;
+  openPanel.canChooseFiles = NO;
+  openPanel.allowsMultipleSelection = NO;
+  if ([openPanel runModal] == NSFileHandlingPanelOKButton) {
+    return [openPanel URLs][0];
+  } else {
+    return nil;
+  }
 }
 
 @end
